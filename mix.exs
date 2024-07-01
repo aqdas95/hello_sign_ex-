effa4ab51791f5297d9ebab9dd8c00995b10b9b2
Aqdas Malik <aqdasmalikcs@gmail.com>
2024-07-01 16:46:54 +0500

    New release tag added

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hellosign.MixProject do
   def project do
     [
       app: :hellosign_ex,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.15",
       description: description(),
       start_permanent: Mix.env() == :prod,
